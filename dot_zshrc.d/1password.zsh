f2d943f4b92a181f47f221d11f983ca3c94c0cdf
Patrick Reagan <patrick.reagan@ideonapi.com>
2023-01-18 12:10:10 -0700

    Allow for listing passwords and ease pass fetching

--- dot_zshrc.d/1password.zsh
+++ dot_zshrc.d/1password.zsh
@@ -3,15 +3,37 @@ function pass-signin() {
   [[ $? != 0 ]] && eval $(op signin)
 }
 
+function pass-list() {
+  pass-signin
+
+  out=$(op item list \
+    --categories Login \
+    --vault Private \
+    --format json | jq -r '.[].title' | sort | sed -e 's/^/  * /')
+
+  echo "Available passwords:"
+  echo $out
+
+}
+
 function pass() {
   set -o pipefail
 
   pass-signin
 
+  if [[ $1 = "" ]]; then
+    pass-list
+    return 1
+  fi
+
   op read op://Private/$1/password 2>/dev/null | pbcopy
 
   if [ $? != 0 ]; then
     >&2 echo "Password not found for $1"
+
+    echo
+
+    pass-list
     return 1
   else
     echo "Password copied to clipboard"
