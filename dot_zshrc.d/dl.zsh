4494f561ed1efa7875672715e77825203a0e52b6
Patrick Reagan <github@the-reagans.com>
2023-10-12 21:01:09 -0600

    Add `playlist-dl` function

--- dot_zshrc.d/dl.zsh
+++ dot_zshrc.d/dl.zsh
@@ -10,3 +10,16 @@ function audio-dl() {
   -o "%(title)s.%(ext)s" \
   $1
 }
+
+function playlist-dl() {
+  youtube-dl \
+  --audio-format mp3 \
+  --postprocessor-args "-id3v2_version 3" \
+  --extract-audio \
+  --embed-thumbnail \
+  --add-metadata \
+  --no-overwrites \
+  --continue \
+  -o "%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s" \
+  $1
+}
