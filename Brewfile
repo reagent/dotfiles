b7622da4c63f5b15e8a01cada54ab56c4f792ec6
Patrick Reagan <github@the-reagans.com>
2023-12-26 12:43:05 -0700

    Add baseline brew dependencies and init function

---
+++ Brewfile
@@ -0,0 +1,18 @@
+# development tools
+brew "git"
+brew "jq"
+brew "rbenv"
+brew "ruby-build"
+brew "nvm"
+brew "yarn"
+
+# persistence
+brew "postgresql"
+brew "redis"
+brew "sqlite"
+
+# multimedia tools
+brew "yt-dlp"
+brew "ffmpeg"
+brew "handbrake"
+brew "lame"
---
+++ dot_zshrc.d/init.zsh
@@ -0,0 +1,16 @@
+function dotfiles-init() {
+  # install homebrew and chezmoi if needed
+  if (( ! $+commands[brew] )); then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+
+  if (( ! $+commands[chezmoi])); then
+    brew install chezmoi
+  fi
+
+  # get dotfiles and update dependencies
+  chezmoi init reagent --apply
+  brew bundle
+
+  source ~/.zshrc
+}
