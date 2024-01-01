57722fc383333eca7a377ad533e64ae7871ec294
Patrick Reagan <github@the-reagans.com>
2024-01-01 12:06:16 -0700

    Only define pass-* functions if we have 1pw CLI

--- dot_zshrc.d/1password.zsh
+++ dot_zshrc.d/1password.zsh
@@ -1,57 +1,59 @@
-function pass-signin() {
-  op whoami >/dev/null 2>&1
-  [[ $? != 0 ]] && eval $(op signin)
-}
+if (( $+commands[op] )); then
+  function pass-signin() {
+    op whoami >/dev/null 2>&1
+    [[ $? != 0 ]] && eval $(op signin)
+  }
 
-function pass-list() {
-  pass-signin
+  function pass-list() {
+    pass-signin
 
-  out=$(op item list \
-    --categories Login \
-    --vault Private \
-    --format json | jq -r '.[].title' | sort | sed -e 's/^/  * /')
+    out=$(op item list \
+      --categories Login \
+      --vault Private \
+      --format json | jq -r '.[].title' | sort | sed -e 's/^/  * /')
 
-  echo "Available passwords:"
-  echo $out
-}
+    echo "Available passwords:"
+    echo $out
+  }
 
-function pass() {
-  set -o pipefail
+  function pass() {
+    set -o pipefail
 
-  pass-signin
+    pass-signin
 
-  if [[ $1 = "" ]]; then
-    pass-list
-    return 1
-  fi
+    if [[ $1 = "" ]]; then
+      pass-list
+      return 1
+    fi
 
-  op read -n op://Private/$1/password 2>/dev/null | pbcopy
+    op read -n op://Private/$1/password 2>/dev/null | pbcopy
 
-  if [ $? != 0 ]; then
-    >&2 echo "Password not found for $1"
+    if [ $? != 0 ]; then
+      >&2 echo "Password not found for $1"
 
-    echo
+      echo
 
-    pass-list
-    return 1
-  else
-    echo "Password copied to clipboard"
-  fi
-}
+      pass-list
+      return 1
+    else
+      echo "Password copied to clipboard"
+    fi
+  }
 
-function pass-create() {
-  pass-signin
+  function pass-create() {
+    pass-signin
 
-  op item create \
-    --category=login \
-    --vault=Private \
-    --generate-password='16,letters,digits,symbols' \
-    --title="$1" >/dev/null 2>&1
+    op item create \
+      --category=login \
+      --vault=Private \
+      --generate-password='16,letters,digits,symbols' \
+      --title="$1" >/dev/null 2>&1
 
-  if [ $? = 0 ]; then
-    pass $1
-  else
-    >&2 echo "Could not save password"
-    return 1
-  fi
-}
+    if [ $? = 0 ]; then
+      pass $1
+    else
+      >&2 echo "Could not save password"
+      return 1
+    fi
+  }
+fi
